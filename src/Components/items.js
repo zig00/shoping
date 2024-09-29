ab995c59bc22adfc0747651b7c93ce59c4a106b7
zig00 <73509141+zig00@users.noreply.github.com>
2024-09-29 14:26:38 +0200

    Update items.js

--- src/Components/items.js
+++ src/Components/items.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 import ItemModal from '../Components/ItemModal'
-import iteminner from '../pages/iteminner'
 import Oneitem from './oneitem'
 const _Items = [
     {
