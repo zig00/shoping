28f82f777c8c83a2e71c61cfdcf8ea68c6ad25c1
zig00 <73509141+zig00@users.noreply.github.com>
2024-09-29 14:27:49 +0200

    Update Mainpage.js

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -1,11 +1,11 @@
-import React, { useState }  from 'react'
+import React  from 'react'
 import Adv1 from './Components/Adv1'
 import Partners from './Components/partners'
 import Items from './Components/items'
 
 function Mainpage() {
   return (
-    <div>Mainpage
+    <div>
      <Adv1/>
      <Partners/>
      <Items/>
